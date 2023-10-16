5a1dde8c2a2fe900b7a28918c27570f39410db30
João Vitor Muniz Vasconcelos <140927075+JoaoVitorMuniz16@users.noreply.github.com>
2023-10-16 14:55:07 -0300

    Add files via upload

---
+++ joaovitor7.py
@@ -0,0 +1,11 @@
+real = float(input("Digite o Valor em Real"))
+dolar = float(input("Digite a cotação do dia!"))
+
+convesao = real / dolar
+
+print(f"""
+    Seu Valor em R$ {real}
+    Cotação atual em $ {dolar}
+
+    Valor de Convesão $ {convesao}
+""")
